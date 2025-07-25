ebad7ef8bd880a7d9875601726cca5e0328ffc26
Alex Karobia <akarobia30@gmail.com>
2025-07-25 13:16:40 +0300

    setting up the project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { FaBeer } from "react-icons/fa"
+
+const App = () => {
+  return (
+    <h1 className="text-green-900 text-2xl">
+       Let's go for a <FaBeer />? 
+    </h1>
+  )
+}
+export default App
